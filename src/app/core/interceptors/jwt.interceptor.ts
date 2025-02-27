fb2a9e1da17f19bb9b25f687cb3d7c0d0eb8a856
= <dinhhoang210903@gmail.com>
2025-02-27 21:40:30 +0700

    (fix): require login when token expires

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -3,9 +3,12 @@ import { inject } from '@angular/core';
 import { AccountService } from '../services/account.service';
 import { User } from '../../models/user';
 import { take } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const accountService = inject(AccountService);
+  const router = inject(Router);
   let currentUser: User;
 
   accountService.currentUser$.pipe(take(1)).subscribe( user => {
@@ -13,11 +16,24 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   });
 
   if(currentUser) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${currentUser.token}`
+    if(currentUser.token) {
+      let decodedToken = jwtDecode(currentUser.token);
+      const isExpired = 
+        decodedToken && decodedToken.exp ?
+        decodedToken.exp < Date.now() / 1000
+        : false;
+
+      if(isExpired) {
+        accountService.logout();
+        router.navigateByUrl('account/login');
+      } else {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${currentUser.token}`
+          }
+        })
       }
-    })
+    }
   }
 
   return next(req);
